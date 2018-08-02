063fed4180f3f70daa5f3c02ed1fd28b215aece8
Jane Jane <janejane@Janes-MacBook-Pro.local>
2018-08-02 00:15:37 -0700

    Added problem#15 template

---
+++ 15_3Sum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  15_3Sum
+//
+//  Created by Jane Jane on 8/2/18.
+//  Copyright © 2018 Jane Jane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! #15\n";
+    return 0;
+}
