9d48497747a04583654d85918e3206c35ac10e5c
Jane Jane <janejane@Janes-MacBook-Pro.local>
2018-08-08 00:09:49 -0700

    Start Problem #70

---
+++ 70_ClimbingStairs/main.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  70_ClimbingStairs
+//
+//  Created by Jane Jane on 8/7/18.
+//  Copyright © 2018 Jane Jane. All rights reserved.
+//
+
+#include <iostream>
+#include <assert.h>
+
+/*
+ You are climbing a stair case. It takes n steps to reach to the top.
+ 
+ Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ 
+ Note: Given n will be a positive integer.
+ 
+ Example 1:
+ 
+ Input: 2
+ Output: 2
+ Explanation: There are two ways to climb to the top.
+ 1. 1 step + 1 step
+ 2. 2 steps
+ Example 2:
+ 
+ Input: 3
+ Output: 3
+ Explanation: There are three ways to climb to the top.
+ 1. 1 step + 1 step + 1 step
+ 2. 1 step + 2 steps
+ 3. 2 steps + 1 step
+ */
+
+//input: 1 2 3 4 5
+//dp   : 1 2 3 3
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    Solution s;
+
+    //assert(s.maxSubArray(2) == 2);
+    return 0;
+}
